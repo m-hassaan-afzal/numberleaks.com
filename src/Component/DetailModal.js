6fde90fe049104e672394584288d6f6dfee3963c
rashidmehmood145 <rashidmehmoodpadda1994@gmail.com>
2022-09-08 16:47:04 +0500

    ui changings

--- src/Component/DetailModal.js
+++ src/Component/DetailModal.js
@@ -12,7 +12,7 @@ export default function DetailModal(props) {
             aria-labelledby="staticBackdropLabel"
             aria-hidden="true"
             style={{ display: props?.open ? "block" : "none" }} >
-            <div className="modal-dialog modal-dialog-centered modal-lg" >
+            <div className="modal-dialog modal-dialog-centered " >
                 <div className="modal-content">
                     <div className="modal-header" style={{ backgroundColor: "#6d147a", color: "white" }}>
                         <h3 className="modal-title" id="staticBackdropLabel" >Contact Information</h3>
@@ -20,15 +20,12 @@ export default function DetailModal(props) {
                     </div>
                     <div className="modal-body">
                         <h4 className='font-weight-bold'> {`This is Inforamtion about ${props.detail.phone}`}</h4>
-                        <ul class="no-bullets">
+                        <ul className="no-bullets">
                             <li>Name : {props.detail.name}</li>
                             <li>Country : {props.detail.country}</li>
                             <li>Phone : {props.detail.phone}</li>
                             <li>Country Code : {props.detail.code}</li>
                         </ul>
-
-
-
                     </div>
                 </div>
             </div>
